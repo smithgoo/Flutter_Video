903522e3d566edd58ad2c8e06a101247f6985ef3
smithg00Happy <smithg00Happy@gmail.com>
2021-05-04 19:18:16 +0800

    修改listView

--- lib/ViewTools/homepage/homePageList.dart
+++ lib/ViewTools/homepage/homePageList.dart
@@ -1,116 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class HomePageList extends StatefulWidget {
+class HomePageList extends StatelessWidget {
   final List infoList;
-  HomePageList({Key key, this.infoList}) : super(key: key);
+  const HomePageList({Key key, this.infoList}) : super(key: key);
 
-  @override
-  _HomePageListState createState() => _HomePageListState();
-}
-
-class _HomePageListState extends State<HomePageList> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        children: [
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-          Text('1111'),
-        ],
+      height: ScreenUtil().setHeight(1334 - 40 - 60 - 49),
+      margin: EdgeInsets.only(top: 0),
+      child: ListView.builder(
+        itemCount: infoList.length,
+        itemBuilder: (context, index) {
+          return _listInkWell(index);
+        },
+      ),
+    );
+  }
+
+  Widget _listInkWell(int index) {
+    return InkWell(
+      onTap: () {
+        print('点击了');
+      },
+      child: Container(
+        height: ScreenUtil().setHeight(120),
+        child: Text(
+          '${infoList[index]}',
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
